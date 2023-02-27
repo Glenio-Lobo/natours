1fd0d1248bb4df227ec6f4a3bacdbfa364deaa64
gleniolobo <gleniolobo2009@gmail.com>
2023-02-26 21:59:06 -0300

    Modelando overview

--- controller/viewController.js
+++ controller/viewController.js
@@ -1,9 +1,17 @@
+import { Tour } from "../models/tourModel.js";
+import { catchAsync } from "../utils/catchAsync.js";
 
-export const getOverview = function(request, response){
+export const getOverview = catchAsync(async function(request, response, next){
+    // 1) Obtenha os dados da coleção
+    const tours = await Tour.find();
+
+    // 2) Crie o template
+    // 3) Renderize o template
     response.status(200).render('overview', {
-        title: 'All Tours'
+        title: 'All Tours',
+        tours
     })
-}
+})
 
 export const getTour = function(request, response){
     response.status(200).render('tour', {
